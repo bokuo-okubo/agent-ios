d77ba2df0bf0c42ac184462332f0f3f80a01cd80
Yohei Okubo <okubo.yohei@gmail.com>
2015-12-20 20:29:58 +0900

    change start script confirm swiftlint is exist bundle with -j4

--- start.sh
+++ start.sh
@@ -13,9 +13,18 @@ invoke() {
     separator
 }
 
+## is exist swiftlint
+if ! which swiftlint >/dev/null; then
+    echo "SwiftLint does not exist, download from https://github.com/realm/SwiftLint"
+    separator
+else
+    echo "SwiftLint already exists! You can use linter!"
+    separator
+fi
+
 ## install gem cocoapods
 if [ ! -e $bundle_path ]; then
-    invoke "bundle install --path vendor/bundle"
+    invoke "bundle install --path vendor/bundle -j4"
 fi
 
 ## install pods
