312278b3e6d5b64110952f6d216553ccfc499d60
Yohei Okubo <okubo.yohei@gmail.com>
2015-12-21 17:39:07 +0900

    fix test.sh xcprettyにパイプで処理渡す

--- test.sh
+++ test.sh
@@ -1,2 +1,4 @@
+#!/bin/sh
+
 xcodebuild test -workspace agent-ios.xcworkspace -scheme agent-ios \
--destination 'platform=iOS Simulator,OS=9.2,name=iPad 2'
+-destination 'platform=iOS Simulator,OS=9.2,name=iPad 2' | bundle exec xcpretty -c && exit ${PIPESTATUS[0]}
